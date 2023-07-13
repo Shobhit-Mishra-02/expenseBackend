b822a36458d5e41b17d7f62c8766822c977338d4
shobhit <shobhitmishra2002@gmail.com>
2023-07-13 21:38:37 +0530

    added the connectDB method to connect to the mongodb database

--- serve.js
+++ serve.js
@@ -1,10 +1,17 @@
 import app from "./app";
 import * as dotenv from "dotenv";
+import connectDB from "./lib/connectDB";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log("The server is running of localhost:" + port);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("The server is running of localhost:" + port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
