1a01a19473b6c8e73693bad6df87ab7817a2152e
shobhit <shobhitmishra2002@gmail.com>
2023-07-13 22:12:54 +0530

    added controller for updating the user details

--- controllers/user/updateUser.js
+++ controllers/user/updateUser.js
@@ -1,9 +1,35 @@
-// For updating the existing users
+import User from "../../lib/schema/User.schema";
 
-const updateUser = (req, res) => {
-  res.status(200).json({
-    message: "this is for updating the user",
-  });
+const updateUser = async (req, res) => {
+  if (req.method === "POST") {
+    const { id } = req.params;
+    const userDataToUpdate = req.body;
+
+    const updateResult = await User.updateOne(
+      {
+        _id: id,
+      },
+      userDataToUpdate
+    );
+
+    if (updateResult.modifiedCount == 1) {
+      res.status(200).json({
+        message: "User updated",
+        status: "success",
+        updateResult,
+      });
+    } else {
+      res.status(404).json({
+        message: "No update",
+        status: "error",
+      });
+    }
+  } else {
+    res.status(405).json({
+      message: "Method not allowed",
+      status: "error",
+    });
+  }
 };
 
 export default updateUser;
