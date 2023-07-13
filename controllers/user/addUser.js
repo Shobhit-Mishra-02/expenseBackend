dc6dc6c07bc5f27249ba1469de31036dee5b285c
shobhit <shobhitmishra2002@gmail.com>
2023-07-13 21:54:09 +0530

    added controller to add new users

--- controllers/user/addUser.js
+++ controllers/user/addUser.js
@@ -1,9 +1,28 @@
-// For creating new users
+import User from "../../lib/schema/User.schema";
 
-const addUser = (req, res) => {
-  res.status(200).json({
-    message: "This is for adding new user",
-  });
+const addUser = async (req, res) => {
+  if (req.method == "POST") {
+    const userDetails = req.body;
+
+    try {
+      const newUser = await User.create(userDetails);
+
+      res.status(201).json({
+        message: "User created successfully",
+        status: "success",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal server error",
+        status: "error",
+      });
+    }
+  } else {
+    res.status(405).json({
+      message: "Method not allowed",
+      status: "error",
+    });
+  }
 };
 
 export default addUser;
