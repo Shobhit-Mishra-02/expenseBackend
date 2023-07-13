86410726916ab90f8bdfed84687a985584d1f5e5
shobhit <shobhitmishra2002@gmail.com>
2023-07-13 22:01:12 +0530

    created a new controller for validating the user, with email and password

--- controllers/user/validateUser.js
+++ controllers/user/validateUser.js
@@ -1,9 +1,36 @@
-// For validating the user
+import User from "../../lib/schema/User.schema.js";
 
-const validateUser = (req, res) => {
-  res.status(200).json({
-    message: "this is for validating the user",
-  });
+const validateUser = async (req, res) => {
+  if (req.method == "POST") {
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email, password }, { password: 0 });
+
+      if (user) {
+        res.status(200).json({
+          message: "User found",
+          status: "success",
+          user,
+        });
+      } else {
+        res.status(404).json({
+          message: "User not found",
+          status: "error",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal server error",
+        status: "error",
+      });
+    }
+  } else {
+    res.status(405).json({
+      message: "Method not allowed",
+      status: "error",
+    });
+  }
 };
 
 export default validateUser;
