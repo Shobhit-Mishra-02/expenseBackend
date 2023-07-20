f89b22563f6b2d3bfd69321f64b5ef0c57761859
shobhit <shobhitmishra2002@gmail.com>
2023-07-20 11:33:13 +0530

    added a new controller to get user by email

--- router/user.route.js
+++ router/user.route.js
@@ -3,6 +3,7 @@ import addUser from "../controllers/user/addUser";
 import validateUser from "../controllers/user/validateUser";
 import updateUser from "../controllers/user/updateUser";
 import getUserById from "../controllers/user/getUserById";
+import getUserByEmail from "../controllers/user/getUserByEmail";
 
 const userRouter = Router();
 
@@ -10,5 +11,6 @@ userRouter.post("/add", addUser);
 userRouter.post("/validate", validateUser);
 userRouter.post("/update/:id", updateUser);
 userRouter.get("/get/:id", getUserById);
+userRouter.get("/get/email/:email", getUserByEmail);
 
 export default userRouter;
